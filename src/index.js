9618050dcaebad7b681617266c882a6163b642eb
Bria222 <techconsultants.bree@gmail.com>
2023-02-01 15:24:16 +0300

    library for Form validation, so we’re gonna add formik

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
