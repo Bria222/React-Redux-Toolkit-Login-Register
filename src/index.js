c8aa41fa3bb48facc991411cd690593e3c0fb1c0
Bria222 <techconsultants.bree@gmail.com>
2023-02-01 15:53:59 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import "./index.css";
 import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -14,3 +14,8 @@ root.render(
     <App />
   </Provider>,
 );
+
+// If you want your app to work offline and load faster, you can chađinge
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,9 +1,13 @@
 export default function authHeader() {
-    const user = JSON.parse(localStorage.getItem('user'));
-  
-    if (user && user.accessToken) {
-      return { Authorization: 'Bearer ' + user.accessToken };
-    } else {
-      return {};
-    }
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  if (user && user.accessToken) {
+    // For Spring Boot back-end
+    // return { Authorization: "Bearer " + user.accessToken };
+
+    // for Node.js Express back-end
+    return { "x-access-token": user.accessToken };
+  } else {
+    return {};
   }
+}
